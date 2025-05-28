acae0d7b9eb9566c30683b2e319e0ec5b2967b6f
Ryan Williams <ryan.williams@kellogg.northwestern.edu>
2025-05-28 19:38:33 +0000

    Debugging before logging out

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,14 +4,17 @@ class UsersController < ApplicationController
     # renders signup form
   end
 
-  def create
-    @user = User.new
-    @user["username"] = params["username"]
-    @user["email"] = params["email"]
-    @user["password"] = params["password"]
-    @user.save
-    redirect_to "/login"
-  end
+def create
+  @user = User.new
+  @user["username"] = params["username"]
+  @user["email"] = params["email"]
+  @user.password = params["password"]  # must use dot notation for bcrypt to save a secure hashed password and save the user
+  @user.save
+  redirect_to "/login"
+end
+
+
+
   
 end
 
